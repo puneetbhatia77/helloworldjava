9a6f733c7689eef914b5f398962ae2049c0e3e75
root <root@u64>
2019-01-13 02:21:15 +0000

    ssh key test

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from Jenkins by both...");
+    System.out.println("Hello World from Jenkins with ssh keys...");
     }
 }
 
