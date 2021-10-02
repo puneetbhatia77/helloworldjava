34ade8157c9c9f7660831d256a4bdbdd9e49188a
puneetbhatia77 <puneetbhatia77@gmail.com>
2021-10-02 12:26:54 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from Jenkins!!");
+    System.out.println("Hello World from Jenkins to test polling!!");
     }
 }
 
