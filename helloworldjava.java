aa4733d5016ee4bb210215272470bdfd3d6c0100
puneetbhatia77 <puneetbhatia77@gmail.com>
2021-11-14 22:16:44 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from DevOps!");
+    System.out.println("Hello World from DevOps to test polling");
     }
 }
 
