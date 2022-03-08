02fa2e116a0662d72040061b7798fb662eb00286
puneetbhatia77 <puneetbhatia77@gmail.com>
2022-03-08 09:47:56 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from DevOps - webhook test");
+    System.out.println("Hello World from DevOps");
     }
 }
 
